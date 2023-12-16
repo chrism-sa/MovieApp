e6cc486c9be7609e634ee10056ba49cc5790f2e9
Charavor.vee <136077569+chrism-sa@users.noreply.github.com>
2023-12-16 14:09:07 +0700

    Update movies_repository.dart

--- lib/repositories/movies_repository.dart
+++ lib/repositories/movies_repository.dart
@@ -1,6 +1,5 @@
 import '../models/movies_model.dart';
 
 abstract class MoviesRepository {
-  //Future porque vamos requisitar a ação a API e não sabemos quando ela vai retornar para a gente
   Future<Movies> getMovies();
 }
